56af6e40139948c375f7781e146c22a86f023a22
rpizzicannella-lgtm <rpizzicannella@gmail.com>
2026-03-25 14:39:45 +0100

    Primo Caricamento

---
+++ sw.js
@@ -0,0 +1,36 @@
+
+const CACHE_NAME = "canyon-quiz-v1-0-0";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./manifest.json",
+  "./js/config.js",
+  "./js/labels.js",
+  "./js/rules.js",
+  "./js/scenarios.js",
+  "./js/app.js",
+  "./data/manual-scenarios.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
