d20ad49b3ae6f947d684e0dd45a1115ad8b14b11
rpizzicannella-lgtm <rpizzicannella@gmail.com>
2026-03-25 17:13:01 +0100

    v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "canyon-quiz-v12";
+const CACHE_NAME = "canyon-quiz-v13";
 const FILES = ["./","./index.html","./admin.html","./styles.css","./manifest.json","./js/config.js","./js/labels.js","./js/rules.js","./js/scenarios.js","./js/quiz-app.js","./js/admin-app.js","./data/manual-scenarios.json"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
